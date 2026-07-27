e4edceaa531200df56d4570aef89aee35988dcf6
samashourbagyy <samashourbagy12@gmail.com>
2026-07-27 12:03:46 +0300

    edit sql schema

--- src/main/java/com/ejada/job_portal/security/SecurityConfig.java
+++ src/main/java/com/ejada/job_portal/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().permitAll()
                 )
 
